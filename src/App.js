43838a0f9d84529505d35ae28725aff9407f3be5
karwa <neeraj>
2026-02-13 20:05:16 +0530

    react app created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to My React App!</h1>
+      <p>This app is ready to test with AWS CodeBuild and CodePipeline.</p>
+    </div>
+  );
+}
+
+export default App;
